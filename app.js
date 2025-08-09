7eec33e3546cd88ffda90c8409592604d36fb66e
Ludania-prog <ludania.pris@gmail.com>
2025-08-09 18:51:35 +0100

    Addes js and API
    Replace the html data to the API data and add a  29 line to replace the fake data that i put at html. HTML lines 30 and 38 had an alteration also. Hide the name of the city and the temperature

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+function refreshweather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.main.temp;
+
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.name;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "4612225ecd1b2c0b68cbfbb8d5268590";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(refreshweather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
